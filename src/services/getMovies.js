8f57c1862a9cb7412a5d57f8b599824409abf643
jack murrie <jackcmurrie4@gmail.com>
2020-10-01 23:03:12 +1000

    moved data file

--- src/services/getMovies.js
+++ src/services/getMovies.js
@@ -1,4 +1,4 @@
 export async function getMovies() {
-    const response = await fetch('https://asia-east2-csesocworkshop.cloudfunctions.net/api/getMovies');
-    return await response.json();
+    const data = require('../dummy_data.json');
+    return data;
 }
