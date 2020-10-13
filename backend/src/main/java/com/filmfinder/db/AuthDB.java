42a37feeb15be6707c0ab7c4fd97e9d224cc5b19
Hikari <hikarihashida7@gmail.com>
2020-10-13 19:42:02 +1100

    Basic route path implemented

--- backend/src/main/java/com/filmfinder/db/AuthDB.java
+++ backend/src/main/java/com/filmfinder/db/AuthDB.java
@@ -2,6 +2,16 @@ package com.filmfinder.db;
 
 public class AuthDB {
 
-    
+    static public void putCredentials(String firstName, String lastName, String email, int hashedPassword) {
+        return;
+    }
+
+    static public String getEmail(String handle) {
+        return "user@gmail.com";
+    }
+
+    static public int getHashedPassword(String handle) {
+        return "password".hashCode();
+    }
     
 }
