f4bede3be0090d5d1e4013684926af3edd57434d
piescotch <z5114240@unsw.edu.au>
2020-10-21 02:10:20 +1100

    Backend: Send auth_token as cookie in response
    When the user has submitted correct login information, send a cookie
    labelled "auth_token" that can be stored by the browser. As long as the
    client has this cookie, they have been successfully authenticated and
    can access their personal user features such as dashboard,
    recommendations, etc.

--- backend/src/main/java/com/filmfinder/resources/ResourceLogin.java
+++ backend/src/main/java/com/filmfinder/resources/ResourceLogin.java
@@ -4,6 +4,7 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.core.NewCookie;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
@@ -44,8 +45,10 @@ public class ResourceLogin {
             // throw exception.
             return Response.status(400).entity("Login unsuccessful\n").build();
         } else {
-            return Response.status(200).entity("Login successful\n").build();
+            return Response.status(200)
+                    .entity("Login successful\n")
+                    .cookie(new NewCookie("auth_token", token))
+                    .build();
         }
     }
-
 }
