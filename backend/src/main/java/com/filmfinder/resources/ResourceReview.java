5d0d2f71c6c9982aa0a6d6bcc2f4f9c5c95a56c9
Hikari <hikarihashida7@gmail.com>
2020-10-22 07:43:54 +1100

    Review feature endpoint implemented with cookie auth.

--- backend/src/main/java/com/filmfinder/resources/ResourceReview.java
+++ backend/src/main/java/com/filmfinder/resources/ResourceReview.java
@@ -2,6 +2,7 @@ package com.filmfinder.resources;
 
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.CookieParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
@@ -10,28 +11,37 @@ import javax.ws.rs.DELETE;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.MediaType;
 
+import com.filmfinder.auth.CredentialHandler;
 import com.filmfinder.db.ReviewDB;
 import com.filmfinder.templates.ReviewTemplate;
 
 @Path("movies/{movieId}")
 public class ResourceReview {
 
+    @CookieParam("auth_token")
+    private String token;
+
     @POST
     @Path("/review")
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
     public Response addReview(@PathParam("movieId") int id, ReviewTemplate review) throws Exception {
-
+        String userEmail = null;
+        try {
+            userEmail = CredentialHandler.decodeToken(token);
+        } catch (Exception e) {
+            return Response.status(400).entity("invalid token.").build();
+        }
 
         String comment = review.getComment();
         float star = review.getStar();
-        // try {
-        //      String userEmail = CredentialHandler.decodeToken(token);
-        //     ReviewDB.putReview(userEmail, id, comment, star);
-        // } catch (Exception e) {
-
-        // }
-        return Response.status(200).entity("todo").build();        
+        try {
+            ReviewDB.putReview(userEmail, id, comment, star);
+            return Response.status(200).entity("Review successfully written.").build();  
+        } catch (Exception e) {
+            return Response.status(400).entity("Review was not written.").build();  
+        }
+              
     }
 
     @PUT
@@ -40,16 +50,21 @@ public class ResourceReview {
     @Produces(MediaType.APPLICATION_JSON)
     public Response editReview(@PathParam("movieId") int id,  ReviewTemplate review) throws Exception {
 
+        String userEmail = null;
+        try {
+            userEmail = CredentialHandler.decodeToken(token);
+        } catch (Exception e) {
+            return Response.status(400).entity("invalid token.").build();
+        }
+
         String comment = review.getComment();
         float star = review.getStar();
-        
-        // try {
-        // String userEmail = CredentialHandler.decodeToken(token);
-        //     ReviewDB.editReview(userEmail, id, comment, star);
-        // } catch (Exception e) {
-
-        // }
-        return Response.status(200).entity("todo").build();        
+        try {
+            ReviewDB.editReview(userEmail, id, comment, star);
+            return Response.status(200).entity("Review edited.").build();        
+        } catch (Exception e) {
+            return Response.status(400).entity("Review was not edited.").build();
+        }
     }
 
     @DELETE
@@ -58,14 +73,20 @@ public class ResourceReview {
     @Produces(MediaType.APPLICATION_JSON)
     public Response removeReview( @PathParam("movie_id") int id, ReviewTemplate review) throws Exception {
         
-        int reviewId = review.getReviewId();
-        // try {
-            // String userEmail = CredentialHandler.decodeToken(token);
-        //     ReviewDB.removeReview(userEmail, id);
-        // } catch (Exception e) {
+        String userEmail = null;
+        try {
+            userEmail = CredentialHandler.decodeToken(token);
+        } catch (Exception e) {
+            return Response.status(400).entity("invalid token.").build();
+        }
 
-        // }        
-        return Response.status(200).entity("todo").build();        
+        // int reviewId = review.getReviewId();
+        try {
+            ReviewDB.removeReview(userEmail, id);
+            return Response.status(200).entity("Review was removed.").build();        
+        } catch (Exception e) {
+            return Response.status(400).entity("Review was not removed.").build();        
+        }        
     }
 
 }
