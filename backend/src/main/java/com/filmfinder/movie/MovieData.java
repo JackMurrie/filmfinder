1ed444d885d5a747852575c55a52a30137c5bfea
kakrial <kristian.fiebig@gmail.com>
2020-10-21 20:03:10 +1100

    restructured branch

---
+++ backend/src/main/java/com/filmfinder/movie/MovieData.java
@@ -0,0 +1,21 @@
+package com.filmfinder.movie;
+
+public class MovieData {
+    String name;
+    String description;
+
+    String director;
+    String imageUrl;
+
+    public MovieData(String _name, String _description, String _director, String _imageUrl) {
+
+        name = _name;
+        description = _description;
+        director = _director;
+        imageUrl = _imageUrl;
+    }
+
+    public String toString() {
+        return "Movie: " + this.name + "\n Description: " + this.description + "\n Director: " + this.director + "\n Image URL: " + this.imageUrl;
+    }
+}
