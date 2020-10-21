9a1a3b30bbf5501f23e7323149faa14bb596ad76
Hikari <hikarihashida7@gmail.com>
2020-10-21 11:48:23 +1100

    Login merged with review

--- backend/src/main/java/com/filmfinder/resources/ResourceLogin.java
+++ backend/src/main/java/com/filmfinder/resources/ResourceLogin.java
@@ -4,8 +4,8 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.Consumes;
-// import javax.ws.rs.core.MediaType;
-// import javax.ws.rs.core.Response;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import io.jsonwebtoken.SignatureAlgorithm;
 
