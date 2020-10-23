dc51e423f02aa96b9d4e7b4acb35743386ac7e27
kakrial <kristian.fiebig@gmail.com>
2020-10-23 13:11:12 +1100

    Added movie DB tests

---
+++ backend/src/test/java/com/filmfinder/db/MovieTest.java
@@ -0,0 +1,16 @@
+package com.filmfinder.db;
+
+import org.junit.Test;
+
+public class MovieTest {
+    @Test
+    public void testGetMovies() {
+        try {
+            System.out.println(MovieDb.getMoviesNoDirector());
+
+        } catch (Exception e) {
+
+        }
+    }
+    
+}
