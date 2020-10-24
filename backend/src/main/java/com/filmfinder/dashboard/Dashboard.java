a6cb529c989894eef86c92be76d2b58c9d4701b2
kakrial <kristian.fiebig@gmail.com>
2020-10-24 22:09:54 +1100

    Exposed all endpoints

--- backend/src/main/java/com/filmfinder/dashboard/Dashboard.java
+++ backend/src/main/java/com/filmfinder/dashboard/Dashboard.java
@@ -5,24 +5,28 @@ import java.util.ArrayList;
 import com.filmfinder.frontendObject.frontendObject;
 import com.filmfinder.movie.Movie;
 import com.filmfinder.user.User;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 import com.google.gson.annotations.Expose;
 
 public class Dashboard extends frontendObject {
     @Expose
     private ArrayList<Movie> watchlist;
+    @Expose
     private ArrayList<Movie> wishlist;
+    @Expose
     private ArrayList<Movie> recommedations;
+    @Expose
     private ArrayList<User> blacklisted;
 
     public Dashboard(int userId) {
         //TODO implement functions
+        watchlist = new ArrayList<Movie>();
+        wishlist = new ArrayList<Movie>();
+        recommedations = new ArrayList<Movie>();
+        blacklisted = new ArrayList<User>();
         try {
             ArrayList<Integer> wList = new ArrayList<Integer>();
             wList.add(2);
             wList.add(5);
-            watchlist = new ArrayList<Movie>();
             for (Integer i: wList) {
                 watchlist.add(Movie.getMovie(i));
             }
