ad05c953f4a4700b76a87f1340e0d944bd162878
kakrial <kristian.fiebig@gmail.com>
2020-10-25 12:10:34 +1100

    Fixed bugs and made base class

--- backend/src/main/java/com/filmfinder/movieLists/Watchlist.java
+++ backend/src/main/java/com/filmfinder/movieLists/Watchlist.java
@@ -1,45 +1,16 @@
 package com.filmfinder.movieLists;
 
 import java.sql.SQLException;
-import java.util.ArrayList;
 
 import com.filmfinder.db.WatchlistDB;
-import com.filmfinder.frontendObject.frontendObject;
-import com.filmfinder.movie.Movies;
-import com.google.gson.annotations.Expose;
 
 import javassist.NotFoundException;
 
-public class Watchlist extends frontendObject {
-    private WatchlistDB dbUtil;
-    @Expose
-    private Movies movies;
-
+public class Watchlist extends MovieList {
+    
     public Watchlist(int userId) throws NotFoundException, SQLException {
         dbUtil = new WatchlistDB(userId);
         movies = dbUtil.getMovies();
-    }
-
-    public void addMovie(int movieId) throws NotFoundException, SQLException {
-        dbUtil.addToList(movieId);
-        movies = dbUtil.getMovies();
-    }
-
-    public void deleteMovie(int movieId) throws SQLException {
-        dbUtil.deleteFromList(movieId);
-        try {
-            movies = dbUtil.getMovies();
-        } catch (NotFoundException e) {
-            e.printStackTrace();
-        }
-    } 
-
-    public ArrayList<Integer> getMovieList() {
-        return getMovieList();
-    }
-
-    public Movies getMovies() {
-        return movies;
     }
 
 }
--- backend/src/main/java/com/filmfinder/movieLists/Wishlist.java
+++ backend/src/main/java/com/filmfinder/movieLists/Wishlist.java
@@ -1,45 +1,16 @@
 package com.filmfinder.movieLists;
 
 import java.sql.SQLException;
-import java.util.ArrayList;
 
 import com.filmfinder.db.WishlistDB;
-import com.filmfinder.frontendObject.frontendObject;
-import com.filmfinder.movie.Movies;
-import com.google.gson.annotations.Expose;
 
 import javassist.NotFoundException;
 
-public class Wishlist extends frontendObject {
-    private WishlistDB dbUtil;
-    @Expose
-    private Movies movies;
+public class Wishlist extends MovieList {
 
     public Wishlist(int userId) throws NotFoundException, SQLException {
         dbUtil = new WishlistDB(userId);
         movies = dbUtil.getMovies();
-    }
-
-    public void addMovie(int movieId) throws NotFoundException, SQLException {
-        dbUtil.addToList(movieId);
-        movies = dbUtil.getMovies();
-    }
-
-    public void deleteMovie(int movieId) throws SQLException {
-        dbUtil.deleteFromList(movieId);
-        try {
-            movies = dbUtil.getMovies();
-        } catch (NotFoundException e) {
-            e.printStackTrace();
-        }
-    } 
-
-    public ArrayList<Integer> getMovieList() {
-        return getMovieList();
-    }
-
-    public Movies getMovies() throws NotFoundException, SQLException {
-        return dbUtil.getMovies();
     }
 
 }
