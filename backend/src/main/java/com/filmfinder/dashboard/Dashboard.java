eeb1122283ea03363b1c27b1d889b53a8efcf08a
kakrial <kristian.fiebig@gmail.com>
2020-10-25 12:10:40 +1100

    Implemented dashboard

--- backend/src/main/java/com/filmfinder/dashboard/Dashboard.java
+++ backend/src/main/java/com/filmfinder/dashboard/Dashboard.java
@@ -5,18 +5,22 @@ import java.util.ArrayList;
 
 import com.filmfinder.frontendObject.frontendObject;
 import com.filmfinder.movie.Movies;
+import com.filmfinder.movieLists.MovieListSerializer;
 import com.filmfinder.movieLists.Watchlist;
 import com.filmfinder.movieLists.Wishlist;
 import com.filmfinder.user.User;
 import com.filmfinder.user.Users;
 import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.JsonAdapter;
 
 import javassist.NotFoundException;
 
 public class Dashboard extends frontendObject {
     @Expose
+    @JsonAdapter(MovieListSerializer.class)
     private Watchlist watchlist;
     @Expose
+    @JsonAdapter(MovieListSerializer.class)
     private Wishlist wishlist;
     @Expose
     private Movies recommedations;
@@ -31,5 +35,10 @@ public class Dashboard extends frontendObject {
         blacklisted = new Users(new ArrayList<User>());
     }
 
+    @Override
+    public String toJson() {
+        // TODO Auto-generated method stub
+        return super.toJson();
+    }
 
 }
