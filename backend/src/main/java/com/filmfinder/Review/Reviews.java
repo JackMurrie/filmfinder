653ea765087635b0e8769a3b2f06f672810cbaa2
kakrial <kristian.fiebig@gmail.com>
2020-10-25 12:11:13 +1100

    Exposed json variable

--- backend/src/main/java/com/filmfinder/Review/Reviews.java
+++ backend/src/main/java/com/filmfinder/Review/Reviews.java
@@ -3,9 +3,11 @@ package com.filmfinder.review;
 import java.util.ArrayList;
 
 import com.filmfinder.frontendObject.frontendObject;
+import com.google.gson.annotations.Expose;
 
 //TODO implement database connection within object
 public class Reviews extends frontendObject {
+    @Expose
     private ArrayList<Review> reviews;
 
     public Reviews(ArrayList<Review> reviews) {
