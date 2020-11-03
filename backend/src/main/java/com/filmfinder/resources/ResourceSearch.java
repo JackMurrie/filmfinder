6c77926724bda3cd94a896870e0a452a1e49234b
Hai Long Bi <hailong.bi2014@gmail.com>
2020-11-03 17:26:15 +1100

    search should now return movie data; also take into account limit of search results

--- backend/src/main/java/com/filmfinder/resources/ResourceSearch.java
+++ backend/src/main/java/com/filmfinder/resources/ResourceSearch.java
@@ -10,9 +10,18 @@ import javax.ws.rs.core.MediaType;
 
 import java.util.HashSet;
 import java.util.Iterator;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
+import com.filmfinder.movie.Movie;
+import com.filmfinder.templates.SearchTemplate;
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+
+import javassist.NotFoundException;
 
 import com.filmfinder.Search.Search;
 import com.filmfinder.movie.Movie;
@@ -26,19 +35,25 @@ public class ResourceSearch {
 
     @POST
     @Path("search")
-    //@Consumes(MediaType.APPLICATION_JSON)
-    //@Produces(MediaType.APPLICATION_JSON)
-    public Response search(String searchString) {
+    // @Consumes(MediaType.APPLICATION_JSON)
+    // @Produces(MediaType.APPLICATION_JSON)
+    public Response search(String searchString) throws NotFoundException, SQLException {
         ArrayList<Integer> results = new ArrayList<Integer>();
         try {
             results = Search.getMovieIdsBySearch(searchString, 10);
         } catch (Exception e) { 
             return Response.status(200).entity("bad search query").build();
         }
+
+        ArrayList<Movie> movieResults = new ArrayList<Movie>();
+
+        for (int i = 0; i < results.size(); i++) {
+            movieResults.add(Movie.getMovie((results.get(i))));
+        }
         JsonObject returnData = new JsonObject();
         Gson gson = new Gson();
-        JsonArray jsonMatches = gson.toJsonTree(results).getAsJsonArray();
-        returnData.add("results", jsonMatches);
+        JsonArray jsonMatches = gson.toJsonTree(movieResults).getAsJsonArray();
+        returnData.add("matchingList", jsonMatches);
         return Response.status(400).entity(gson.toJson(returnData)).build();
         
     }
