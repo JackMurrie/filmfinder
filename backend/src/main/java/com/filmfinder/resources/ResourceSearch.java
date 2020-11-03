843467a7e59108a0d7433fd9ea99f099fcefd91a
kakrial <kristian.fiebig@gmail.com>
2020-11-03 18:57:02 +1100

    Removed unecessary imports

--- backend/src/main/java/com/filmfinder/resources/ResourceSearch.java
+++ backend/src/main/java/com/filmfinder/resources/ResourceSearch.java
@@ -1,35 +1,16 @@
 package com.filmfinder.resources;
 
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
-import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.core.Response;
-import javax.ws.rs.core.MediaType;
 
-import java.util.HashSet;
-import java.util.Iterator;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
-import com.filmfinder.movie.Movie;
 import com.filmfinder.movie.Movies;
-import com.filmfinder.templates.SearchTemplate;
-import com.google.gson.Gson;
-import com.google.gson.JsonArray;
-import com.google.gson.JsonObject;
 
 import javassist.NotFoundException;
 
 import com.filmfinder.Search.Search;
-import com.filmfinder.movie.Movie;
-import com.filmfinder.templates.SearchTemplate;
-import com.google.gson.Gson;
-import com.google.gson.JsonArray;
-import com.google.gson.JsonObject;
 
 @Path("search/")
 public class ResourceSearch {
@@ -42,7 +23,7 @@ public class ResourceSearch {
         try {
             Movies movies = Search.getMovieIdsBySearch(searchString, 10);
 
-        return Response.status(200).entity(movies.toJson()).build();
+            return Response.status(200).entity(movies.toJson()).build();
 
         } catch (Exception e) { 
             return Response.status(400).entity("bad search query").build();
