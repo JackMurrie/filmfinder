207059fca6ee04a7df10b545033c3880c292be3d
kakrial <kristian.fiebig@gmail.com>
2020-11-03 18:58:20 +1100

    Added unecessary imports

--- backend/src/main/java/com/filmfinder/resources/ResourceSearch.java
+++ backend/src/main/java/com/filmfinder/resources/ResourceSearch.java
@@ -1,12 +1,15 @@
 package com.filmfinder.resources;
 
 import javax.ws.rs.Path;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import java.sql.SQLException;
 
 import com.filmfinder.movie.Movies;
+import com.filmfinder.templates.SearchTemplate;
 
 import javassist.NotFoundException;
 
