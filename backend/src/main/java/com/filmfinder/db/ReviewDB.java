530b4088c50286c8b55cd7cd5d89da0304006aa9
kakrial <kristian.fiebig@gmail.com>
2020-11-04 15:13:35 +1100

    Fixed review

--- backend/src/main/java/com/filmfinder/db/ReviewDB.java
+++ backend/src/main/java/com/filmfinder/db/ReviewDB.java
@@ -79,17 +79,16 @@ public class ReviewDB {
 	public static void putReview(String email, int movieId, String comment, float star) throws SQLException {
         Connection c = null;
         PreparedStatement s = null;
-        String reviewString = "";
+        String reviewString = comment;
         try {
             int userId = UtilDB.getUserId(email);
             c = DbDataSource.getConnection();
             try {
                 Review r = getReview(movieId, userId);
                 String oldReview = r.getComment();
-                if (!oldReview.equals("")) {
+                if (!oldReview.equals("") && comment.equals("")) {
                     reviewString = oldReview;
                 }
-                reviewString = comment;
             } catch (Exception e) {}
             String q = "REPLACE INTO review(movie_id, user_id, review, rating) values (?, ?, ?, ?);";
             s = c.prepareStatement(q);
