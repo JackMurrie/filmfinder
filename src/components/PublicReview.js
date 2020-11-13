aaac4b0c796acad220a84eaa3d0e3f6ea954c430
jack murrie <jackcmurrie4@gmail.com>
2020-11-14 00:32:10 +1100

    fixed review details

--- src/components/PublicReview.js
+++ src/components/PublicReview.js
@@ -4,6 +4,8 @@ import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import Paper from '@material-ui/core/Paper';
 import { makeStyles } from '@material-ui/core/styles';
+import Rating from '@material-ui/lab/Rating';
+import Box from '@material-ui/core/Box';
 
 const useStyles = makeStyles((theme) => ({
     '@global': {
@@ -30,10 +32,18 @@ export default function PublicReview(props) {
     return (
         <Grid item xs={12}>
           <Paper className={classes.paper}>
-            {props.text}
+            <div className="text">
+              {props.text}
+            </div>
+            <Box component="fieldset" mb={-1} borderColor="transparent" marginTop={5}>
+              <Rating name="read-only" precision={0.5} value={props.rating} readOnly/>
+            </Box>
             <div className="right">
             <Button href={`/user/${props.user}`} color="secondary">
-                TODO: Post date and user info
+                {props.user}
+            </Button>
+            <Button disabled color="secondary">
+                {props.postDate}
             </Button>
             </div>
           </Paper>
